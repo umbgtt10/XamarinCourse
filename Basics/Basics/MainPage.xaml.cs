53f0679e54965b9d20efa423041dce89e002a357
Umberto <Umbertogotti1979@gmail.com>
2019-01-26 07:20:33 +0100

    Device class for platform-specific setting implemented.

---
+++ Basics/Basics/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Basics
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            switch (Device.RuntimePlatform)
+            {
+                case Device.iOS:
+                    Padding = new Thickness(0, 20, 0, 0);
+                    break;
+
+                case Device.Android:
+                    Padding = new Thickness(0, 0, 0, 0);
+                    break;
+
+                case Device.UWP:
+                    Padding = new Thickness(0, 0, 0, 0);
+                    break;
+            }
+        }
+    }
+}
