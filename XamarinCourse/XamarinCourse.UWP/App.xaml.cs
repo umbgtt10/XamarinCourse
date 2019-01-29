3fdefc6783ec49ee22e4c470d16310bdfffefca4
Umberto <Umbertogotti1979@gmail.com>
2019-01-29 13:42:46 +0100

    Round images integrated.

--- XamarinCourse/XamarinCourse.UWP/App.xaml.cs
+++ XamarinCourse/XamarinCourse.UWP/App.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using ImageCircle.Forms.Plugin.UWP;
 
 namespace XamarinCourse.UWP
 {
@@ -53,6 +54,7 @@ namespace XamarinCourse.UWP
                 rootFrame.NavigationFailed += OnNavigationFailed;
 
                 Xamarin.Forms.Forms.Init(e);
+                ImageCircleRenderer.Init();
 
                 if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
                 {
