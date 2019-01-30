38de90b6b1914a3d17ffaae748c14b7ae2bc3dd6
Umberto <Umbertogotti1979@gmail.com>
2019-01-30 07:22:53 +0100

    Few exercises and few lessons left.

---
+++ XamarinCourse/XamarinCourse/Forms/Exercise/ContactBookExercise.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinCourse.Forms.Exercise
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ContactBookExercise : ContentPage
+	{
+		public ContactBookExercise ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
